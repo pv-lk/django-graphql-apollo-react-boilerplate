467b97e98b1bb1f129e569377d232a7bbf7b7722
e.g. pavelka <elwynpavelka@gmail.com>
2019-03-12 16:32:27 -0400

    added local variables for development in emacs

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;; To run project:
+;; "H-d p s u" (pony-runserver)
+;; "C-c n n"   (npm-init)
+
+((nil . ((mode . npm)
+       (mode . djangonaut)
+       (pyvenv-activate . "/home/gigi/Code/Current/dgar/venv/")
+       (python-shell-process-environment . ("DJANGO_SETTINGS_MODULE=api.settings"))
+       (python-shell-extra-pythonpaths . ("/home/gigi/Code/Current/dgar/api/"))
+       (python-shell-virtualenv-root . "/home/gigi/Code/Current/dgar/venv/"))))
