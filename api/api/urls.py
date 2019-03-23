8213c23f2f0ffa7a44b118c1578c48e207db8013
e.g. pavelka <elwynpavelka@gmail.com>
2019-03-23 01:02:41 -0400

    "added graphql schema"

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls import url
+from django.views.generic import RedirectView
+from graphene_django.views import GraphQLView
+
+
+from .schema import schema
+
+admin.autodiscover()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)))
 ]
