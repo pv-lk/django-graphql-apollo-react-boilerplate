9a6be5fb3913ae516cd8da3ee56fff878de4bb58
e.g. pavelka <elwynpavelka@gmail.com>
2019-04-05 23:18:37 -0400

    set up user schema

--- api/users/schema.py
+++ api/users/schema.py
@@ -1,11 +1,48 @@
+from django.contrib.auth import get_user_model
+
 import graphene
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
 
 class Query(graphene.ObjectType):
-    pass
+    me = graphene.Field(UserType)
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info, **kwargs):
+        return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in')
+
+        return user
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+        firstName = graphene.String()
+
+    def mutate(self, info, username, email, password):
+        user = get_user_model()(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
 
 
 class Mutation(graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+    create_user = CreateUser.Field()
